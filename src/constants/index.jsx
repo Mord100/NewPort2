08aed485f1d86aa34f2383212d7f11582742e7e8
Mordecai Chingolo <138593277+Mord100@users.noreply.github.com>
2026-02-27 10:47:34 +0200

    Remove 'Work' section from navigation
    Removed the 'Work' section from the navigation array.

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -2,22 +2,6 @@ import { SocialMediaProfiles } from "@/components/SocialMedia";
 
 export const navigation = [
   {
-    title: "Work",
-    links: [
-      { title: "Zanyumba", href: "/work" },
-      { title: "Project1", href: "/work" },
-      { title: "Project2", href: "/work" },
-      {
-        title: (
-          <>
-            See all <span aria-hidden="true">&rarr;</span>
-          </>
-        ),
-        href: "/work",
-      },
-    ],
-  },
-  {
     title: "Company",
     links: [
       { title: "About", href: "/about" },
